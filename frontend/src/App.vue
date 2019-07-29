ffefff1b15d305cb8ee7fa1fb3fa18e464d7a016
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2019-07-29 11:42:30 +0300

    fix frontend init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 12 12"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
