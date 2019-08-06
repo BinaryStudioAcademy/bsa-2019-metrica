50c99b6d3f7355cbf66fd51c8cfac15f1fe939fd
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-06 21:32:01 +0300

    edit router

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,10 +12,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::post('/v1/login', 'Api\\Auth\\AuthController@login');
+Route::prefix('v1')->group(function () {
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::post('/v1/login', 'Api\\Auth\\AuthController@login');
+    });
 });
+
 
 
 
