bb51e99ba7de3e511f97861bbe7b1e14ceff1b30
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-10 09:47:16 +0300

    hotfix seeder edit fake name

--- backend/database/seeds/WebsiteTableSeeder.php
+++ backend/database/seeds/WebsiteTableSeeder.php
@@ -24,7 +24,7 @@ class WebsiteTableSeeder extends Seeder
             function (User $user) use ($faker, $now){
                 return factory(Website::class, 1)->make([
                     'user_id' => $user->id,
-                    'name' => $faker->unique()->name,
+                    'name' => $faker->unique()->name. $now->timestamp,
                     'domain' => "http://". $faker->unique()->domainWord. $now->timestamp .".com",
                     'created_at' => $now->toDateTimeString()
                 ]);
