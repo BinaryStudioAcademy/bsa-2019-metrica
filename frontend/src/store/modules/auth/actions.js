6ec675b51cc25ad9eca46a3a573a3a8bb9badafa
SerhiiPlotnikov <serhiiplotnikov1934@gmail.com>
2019-08-10 18:38:39 +0300

    Implemented actions and mutatotrs

--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -1,6 +1,6 @@
-import {LOGIN, LOGOUT, RESET_PASSWORD,GET_USER_DATA, SET_IS_LOGGED_IN} from './types/actions';
+import {LOGIN, LOGOUT, SIGNUP,RESET_PASSWORD,GET_USER_DATA, SET_IS_LOGGED_IN} from './types/actions';
 import {SET_AUTHENTICATED_USER, USER_LOGIN, USER_LOGOUT,SET_USER_IS_LOGGED_IN,SET_AUTHENTICATED} from "./types/mutations";
-import { authorize, getAuthUser } from '@/api/auth';
+import { authorize, getAuthUser, registerUser } from '@/api/auth';
 
 export default {
     [LOGIN]: (context, user) => {
@@ -25,6 +25,10 @@ export default {
         });
     },
 
+    [SIGNUP]: (context, newUser) => {
+        return registerUser(newUser);
+    },
+
     [RESET_PASSWORD]: () => {
         return new Promise((resolve, reject) => {
             const fakeResponse = 201;
@@ -41,8 +45,9 @@ export default {
         })
     },
 
+
     [GET_USER_DATA]: (context) => {
-         getAuthUser().then(response => {
+        getAuthUser().then(response => {
             const user = response.data;
             alert(user.name);
             context.commit(SET_AUTHENTICATED,user);
--- frontend/src/store/modules/auth/mutations.js
+++ frontend/src/store/modules/auth/mutations.js
@@ -1,5 +1,4 @@
-import {SET_AUTHENTICATED_USER, SET_TOKEN, USER_LOGIN, USER_LOGOUT, SET_USER_IS_LOGGED_IN, SET_AUTHENTICATED} from "./types/mutations";
-
+import {SET_AUTHENTICATED_USER, SET_TOKEN, USER_LOGIN, USER_LOGOUT, SET_USER_IS_LOGGED_IN,SET_AUTHENTICATED} from "./types/mutations";
 export default {
     [SET_TOKEN]: (state, token) => {
         state.token = token;
@@ -20,6 +19,7 @@ export default {
         state.isLoggedIn = true;
         state.currentUser = user;
     },
+
     [SET_USER_IS_LOGGED_IN]: (state,data) => {
         state.isLoggedIn = data;
     },
