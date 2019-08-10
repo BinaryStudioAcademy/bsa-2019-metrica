b08118a862fce3660d71ce72675e0e98a0a2cdd5
naytis <binaural.omega@gmail.com>
2019-08-10 17:24:21 +0300

    add UnauthenticatedException

---
+++ backend/app/Exceptions/UnauthenticatedException.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use App\Contracts\ApiException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Response;
+
+final class UnauthenticatedException extends AuthenticationException implements ApiException
+{
+    protected $message = "Unauthenticated.";
+
+    public function __construct(string $message = '', array $guards = [], ?string $redirectTo = null)
+    {
+        parent::__construct($this->message, $guards, $redirectTo);
+    }
+
+    public function getStatus(): int
+    {
+        return Response::HTTP_UNAUTHORIZED;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'message' => $this->message
+        ];
+    }
+}
