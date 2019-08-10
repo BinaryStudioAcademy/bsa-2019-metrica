504c03c239b0a361536a67a2eb590e8cf9893fd2
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-10 23:24:12 +0300

    change redirect from default route

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -15,7 +15,7 @@ export default new Router({
     routes: [
         {
             path: '',
-            redirect: 'login'
+            redirect: 'home'
         },
         {
             path: '/login',
