88e531cf193383d5bf9dfdcf0820ba488be58d8b
Eduard <motrii.edu@gmail.com>
2019-08-10 22:44:55 +0300

    update for passing travis

--- backend/tests/Feature/VisitorsApiTest.php
+++ backend/tests/Feature/VisitorsApiTest.php
@@ -1,14 +1,24 @@
 <?php
 
-namespace tests\Feature;
+namespace Tests\Feature;
 
+use App\Entities\User;
 use Tests\TestCase;
 
 class VisitorsApiTest extends TestCase
 {
     public function testNewVisitorsAction()
     {
-        $response = $this->call('GET', 'api/v1/visitors/new');
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->call('GET', 'api/v1/visitors/new');
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testAllVisitorsAction()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->call('GET', 'api/v1/visitors');
 
         $this->assertEquals(200, $response->getStatusCode());
     }
