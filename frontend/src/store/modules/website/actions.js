7a9b1b207f5f46223907c5549979db6ef14b7ffb
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-12 01:17:14 +0300

    send data to backend (save website)

--- frontend/src/store/modules/website/actions.js
+++ frontend/src/store/modules/website/actions.js
@@ -1,5 +1,9 @@
 import {SAVE_NEW_WEBSITE, SET_WEBSITE_DATA} from './types/actions';
 import {ADD_WEBSITE, SET_WEBSITE_INFO} from "./types/mutations";
+import requestService from "@/services/requestService";
+import config from "@/config";
+
+const resourceUrl = config.getApiUrl() + '/websites';
 
 export default {
     [SET_WEBSITE_DATA]: (context, data) => {
@@ -8,38 +12,8 @@ export default {
     [SAVE_NEW_WEBSITE]: (context) => {
         const newDataSite = context.state.newWebsite;
 
-        return new Promise((resolve, reject) => {
-            if(!newDataSite.name || !newDataSite.domain) {
-                reject({
-                    errors: {
-                        message: "Fill in all thi fields correctly."
-                    }
-                });
-            }
-            const fakeWebsiteData = {
-                name: 'Website name',
-                domain: 'http://domain.com',
-                single_page: newDataSite.single_page,
-                tracking_number: '123456789-10',
-            };
-
-            if (fakeWebsiteData.tracking_number && fakeWebsiteData.name === newDataSite.name && fakeWebsiteData.domain === newDataSite.domain) {
-                context.commit(ADD_WEBSITE, fakeWebsiteData);
-                resolve(fakeWebsiteData);
-            } else if (fakeWebsiteData.name !== newDataSite.name) {
-                reject({
-                    errors: {
-                        name: "Wrong name of website"
-                    }
-                });
-            } else if (fakeWebsiteData.domain !== newDataSite.domain) {
-                reject({
-                    errors: {
-                        domain: "Wrong domain of website"
-                    }
-                });
-            }
-
-        });
+        return requestService.create(resourceUrl, newDataSite)
+                .then( response => context.commit(ADD_WEBSITE, response.data.data))
+                .catch(response => { return { errors: response.error } });
     }
 }
