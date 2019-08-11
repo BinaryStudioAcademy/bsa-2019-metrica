7bafea70841022e1fd5a515a6a7a456c75e41496
Yana Yakovenko <y.yakovenko@lenal.biz>
2019-08-11 15:53:32 +0300

    fix views

---
+++ frontend/src/components/auth/LoginGuard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Sidenav v-if="isLogged" />
+        <VContent v-if="isLogged">
+            <RouterView />
+        </VContent>
+        <LoginForm v-else />
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from "vuex";
+    import {IS_LOGGED_IN} from "@/store/modules/auth/types/getters";
+    import LoginForm from "./LoginForm.vue";
+    import Sidenav from "../layout/Sidepanel.vue";
+
+    export default {
+        components: {
+            LoginForm,
+            Sidenav
+        },
+        computed: {
+            ...mapGetters('auth', {
+                isLogged: IS_LOGGED_IN
+            })
+        }
+    }
+</script>
---
+++ frontend/src/components/layout/header/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+    <VMenu>
+        <template v-slot:activator="{ on }">
+            <VIcon
+                class="drop-down"
+                v-on="on"
+            >
+                arrow_drop_down
+            </VIcon>
+        </template>
+        <VList>
+            <VListItem
+                :key="item"
+                v-for="item in items"
+            >
+                <VListItemTitle>
+                    {{ item }}
+                </VListItemTitle>
+            </VListItem>
+        </VList>
+    </VMenu>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            items: ['profile', 'sign out'],
+            bell: {
+                icon: '/assets/icons/bell.svg'
+            }
+        }),
+    }
+</script>
+
+<style scoped lang="scss">
+.v-icon.drop-down {
+    color: #3C57DE;
+}
+</style>
