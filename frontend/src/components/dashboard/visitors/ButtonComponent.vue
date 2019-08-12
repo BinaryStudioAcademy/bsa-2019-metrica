652ed2d01593687611919f2ce09e0d1f875edcc3
UMarina <m.udovitskaya@gmail.com>
2019-08-12 09:52:02 +0300

    add hover scale transition and border, change computed on created hook

--- frontend/src/components/dashboard/visitors/ButtonComponent.vue
+++ frontend/src/components/dashboard/visitors/ButtonComponent.vue
@@ -46,11 +46,14 @@
                 required: true
             }
         },
-        computed: {
-            iconSrs ()  {
-                return "data:image/png;base64," + getIcon(this.iconKey);
+        data () {
+            return {
+                iconSrs: ''
             }
-        }
+        },
+        created () {
+            this.iconSrs = "data:image/png;base64," + getIcon(this.iconKey);
+        },
     }
 </script>
 
@@ -62,6 +65,7 @@
         border-radius: 6px;
         padding-right: 40px;
         padding-left: 20px;
+        transition: all .2s ease-in;
 
         .card-image {
             margin-right: 30px;
@@ -81,5 +85,11 @@
             line-height: 14px;
             color: rgba(18, 39, 55, 0.5);
         }
+    }
+
+    .button-card:hover {
+        box-shadow: 0px 0px 28px rgba(194, 205, 223, 0.7);
+        border: 1px solid rgba(60, 87, 222, 0.52);
+        transform: scale(1.19);
     }
 </style>
