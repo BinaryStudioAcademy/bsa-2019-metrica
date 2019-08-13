5be5d8885b5c58efb919c3d76aac85dbb34ffc43
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-13 16:17:02 +0300

    add getAvgSession method to SessionRepository

--- backend/app/Repositories/EloquentSessionRepository.php
+++ backend/app/Repositories/EloquentSessionRepository.php
@@ -8,6 +8,8 @@ use App\Repositories\Contracts\SessionRepository;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Carbon;
 use App\Entities\Session;
+use App\Actions\Sessions\GetAvgSessionRequest;
+use App\Entities\Visitor;
 
 class EloquentSessionRepository implements SessionRepository
 {
@@ -16,19 +18,24 @@ class EloquentSessionRepository implements SessionRepository
         return collect([]);
     }
 
-    public function getAvgSession(?int $days = null): int
+    public function getAvgSession(GetAvgSessionRequest $request): int
     {
-        $startDay = Carbon::today()->subDays($days);
 
-        $avgSessionInSeconds = Session::when($days, function($query, $days){
-                    return $query->where('start_session', '<=', $startDay);
-                })
-                ->get(['start_session', 'end_session'])
-                ->map(function ($session) {
-                    return $session->end_session->diffInSeconds($session->start_session);
-                })
-                ->avg();
+        // it will be actual when User->Website relations is changed to OneToOne
+        $websiteId = auth()->user()->website->id;
 
-        return round($avgSessionInSeconds);
+        $visitorsIDsOfWebsite = Visitor::where('website_id', $websiteId)
+                                        ->get(['id']);
+
+        $avgSession = Session::whereIn('visitor_id', $visitorsIDsOfWebsite)
+                        ->where('start_session', '>=', $request->startDate())
+                        ->where('start_session', '<=', $request->endDate())
+                        ->get(['start_session', 'end_session'])
+                        ->map(function ($session) {
+                            return $session->end_session->diffInSeconds($session->start_session);
+                        })
+                        ->avg();
+
+        return round($avgSession);
     }
 }
