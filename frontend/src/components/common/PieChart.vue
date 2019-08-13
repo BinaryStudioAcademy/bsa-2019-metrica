f0ed7e939a64ed4f70c0640584f7966e84759ea2
Andrey Borenko <aborenko1996@gmail.com>
2019-08-13 09:24:02 +0300

    added pie chart component

---
+++ frontend/src/components/common/PieChart.vue
@@ -0,0 +1,44 @@
+<template>
+    <GChart
+        type="PieChart"
+        :data="chartData"
+        :options="chartOptions"
+    />
+</template>
+
+<script>
+    import {GChart} from 'vue-google-charts'
+
+    export default {
+        components: {
+            GChart
+        },
+        props: {
+            title: {
+                type: String,
+                default: 'PieChart'
+            },
+            data: {
+                type: Array,
+                default: () => []
+            },
+            pieHole: {
+                type: Number,
+                default: 0.9
+            }
+        },
+        data() {
+            return {
+                chartData: this.data,
+                chartOptions: {
+                    title: this.title,
+                    pieHole: this.pieHole
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
