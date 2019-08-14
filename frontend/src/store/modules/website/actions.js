4276e1c1611e659247809da85fbbc5dfd3f0cbe6
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-14 03:10:47 +0300

    handle errors and correct dependencies import

--- frontend/src/store/modules/website/actions.js
+++ frontend/src/store/modules/website/actions.js
@@ -1,28 +1,58 @@
 import {SAVE_NEW_WEBSITE, SET_WEBSITE_DATA, UPDATE_WEBSITE} from './types/actions';
-import {ADD_WEBSITE, SET_WEBSITE_INFO, UPDATE_CURRENT_WEBSITE} from "./types/mutations";
+import {SET_WEBSITE_INFO, UPDATE_CURRENT_WEBSITE} from "./types/mutations";
 import { addWebsite } from '@/api/website';
 
 export default {
+
     [SET_WEBSITE_DATA]: (context, data) => {
         context.commit(SET_WEBSITE_INFO, data);
     },
+
     [SAVE_NEW_WEBSITE]: (context) => {
         const newDataSite = context.state.newWebsite;
 
-        return new Promise((resolve, reject) => {
-            if(!newDataSite.name || !newDataSite.domain) {
-                reject({
-                    errors: {
-                        message: "Fill in all thi fields correctly."
+        if(!newDataSite.name || !newDataSite.domain) {
+            throw {
+                errors: {
+                    message: "Fill in all this fields correctly."
+                }
+            };
+        }
+
+        return addWebsite(newDataSite)
+                .then( response => {
+                    context.commit(UPDATE_CURRENT_WEBSITE, response.data)
+                })
+                .catch(error => {
+                    let errorBag = error.response.data.errors;
+
+                    if (errorBag.name) {
+                        throw {
+                            errors: {
+                                name: errorBag.name
+                            }
+                        }
+                    }
+
+                    if (errorBag.domain) {
+                        throw {
+                            errors: {
+                                domain: errorBag.domain
+                            }
+                        }
+                    }
+
+                    if (errorBag && !errorBag.name && !errorBag.domain) {
+                        throw {
+                            errors: {
+                                message: errorBag
+                            }
+                        }
                     }
                 });
-            }
-            resolve();
-            return addWebsite(newDataSite)
-                    .then( response => context.commit(ADD_WEBSITE, response.data.data))
-                    .catch(response => { return { errors: response.error } });
-        });
+
     },
+
     [UPDATE_WEBSITE]: (context, name) => {
         return new Promise((resolve, reject) => {
             const website = {
