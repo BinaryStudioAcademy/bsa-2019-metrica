79863fa6c9b679d5199da2f620f2650d20c24108
Yana Yakovenko <y.yakovenko@lenal.biz>
2019-08-15 10:23:23 +0300

    add semicolons

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     chainWebpack: config => {
         ["vue-modules", "vue", "normal-modules", "normal"].forEach((match) => {
             config.module.rule('scss').oneOf(match).use('sass-loader')
-                .tap(opt => Object.assign(opt, { data: `@import '~@/scss/main.scss';` }))
-        })
+                .tap(opt => Object.assign(opt, { data: `@import '~@/scss/main.scss';` }));
+        });
     }
 };
