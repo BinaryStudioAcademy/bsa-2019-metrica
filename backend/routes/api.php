2b82328daffdba79273b60842cb5a4036dea26bd
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2019-08-15 12:25:52 +0000

    Apply fixes from StyleCI

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -57,7 +57,7 @@ Route::prefix('v1')->group(function () {
 
         Route::group([
            'prefix' => 'visits'
-        ], function(){
+        ], function() {
             Route::get('/', 'VisitController@getPageViews');
         });
     });
