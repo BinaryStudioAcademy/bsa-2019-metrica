5f09880c9dd1866799fa1dd4149ca686ccaaa75d
Stas Kozar <stas.kozar@transoftgroup.com>
2019-08-15 18:49:41 +0300

    Visitors: combine ui components according to design and specification

--- frontend/src/components/layout/Sidepanel.vue
+++ frontend/src/components/layout/Sidepanel.vue
@@ -9,12 +9,15 @@
             class="mt-12"
             flat
         >
-            <RouterLink 
+            <RouterLink
                 v-for="link in links"
                 :key="link.text"
                 :to="{ name: link.route }"
             >
-                <VListItem class="pl-8 my-3">
+                <VListItem
+                    class="pl-8 my-3"
+                    v-if="!link.hasOwnProperty('sublinks')"
+                >
                     <VLayout flex>
                         <VListItemIcon class="py-0 my-0 mr-7">
                             <svg>
@@ -28,6 +31,43 @@
                         </VListItemContent>
                     </VLayout>
                 </VListItem>
+                <VList v-else>
+                    <VListGroup>
+                        <template v-slot:activator>
+                            <VListItem>
+                                <VLayout flex>
+                                    <VListItemIcon class="py-0 my-0 mr-7">
+                                        <svg>
+                                            <use :href="`${link.icon}#root`" />
+                                        </svg>
+                                    </VListItemIcon>
+                                    <VListItemContent class="py-0">
+                                        <VListItemTitle>
+                                            {{ link.text }}
+                                        </VListItemTitle>
+                                    </VListItemContent>
+                                </VLayout>
+                            </VListItem>
+                        </template>
+                        <VList>
+                            <RouterLink
+                                v-for="sublink in link.sublinks"
+                                :key="sublink.text"
+                                :to="{ name: sublink.route }"
+                            >
+                                <VListItem>
+                                    <VLayout flex>
+                                        <VListItemContent>
+                                            <VListItemTitle>
+                                                {{ sublink.text }}
+                                            </VListItemTitle>
+                                        </VListItemContent>
+                                    </VLayout>
+                                </VListItem>
+                            </RouterLink>
+                        </VList>
+                    </VListGroup>
+                </VList>
             </RouterLink>
         </VList>
     </VNavigationDrawer>
@@ -45,7 +85,21 @@
                 {
                     icon: '/assets/icons/person.svg',
                     text: 'Audience',
-                    route: 'visitors'
+                    route: 'visitors',
+                    sublinks: [
+                        {
+                            text: 'Visitors',
+                            route: 'visitors',
+                        },
+                        {
+                            text: 'Page Views',
+                            route: 'visitors',
+                        },
+                        {
+                            text: 'Geo Location',
+                            route: 'visitors',
+                        },
+                    ]
                 },
                 {
                     icon: '/assets/icons/settings.svg',
@@ -70,6 +124,7 @@
 <style scoped lang="scss">
 $blue: #3C57DE;
 $grey: rgba(18, 39, 55, 0.5);
+$black: black;
 
 svg {
     width: 25px;
@@ -104,6 +159,17 @@ a .v-list-item {
         .v-list-item__icon {
             fill: #3C57DE;
             fill-opacity: 1;
+        }
+    }
+}
+
+.router-link-active {
+    .v-list-group__items {
+        .v-list-item {
+            border-left: none;
+            .v-list-item__title {
+                color: $black;
+            }
         }
     }
 }
