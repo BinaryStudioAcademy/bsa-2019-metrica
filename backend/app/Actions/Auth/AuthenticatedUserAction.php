a567dacfa28460277a380b221f01f018593b0690
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-16 11:37:34 +0300

    edit validation for login form

--- backend/app/Actions/Auth/AuthenticatedUserAction.php
+++ backend/app/Actions/Auth/AuthenticatedUserAction.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace App\Actions\Auth;
 
 use App\Exceptions\TokenCouldNotCreate;
-use App\Exceptions\UserByEmailNotFoundException;
+use App\Exceptions\UnauthenticatedException;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -14,13 +14,14 @@ final class AuthenticatedUserAction
     public function execute(AuthenticatedUserRequest $request): AuthenticationResponse
     {
         try {
+
             $token = JWTAuth::attempt([
                 'email' => $request->getEmail(),
                 'password' => $request->getPassword()
             ]);
 
             if (!$token) {
-                throw new UserByEmailNotFoundException();
+                throw new UnauthenticatedException();
             }
         } catch (JWTException $exception) {
             throw new TokenCouldNotCreate();
