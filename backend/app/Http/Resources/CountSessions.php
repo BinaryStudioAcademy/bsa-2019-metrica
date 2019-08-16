a7fe99a9d3d68b624b5a8a8193f0da445f10abc6
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-16 22:39:27 +0300

    fix syntax, dependencies

--- backend/app/Http/Resources/CountSessions.php
+++ backend/app/Http/Resources/CountSessions.php
@@ -3,10 +3,11 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Contracts\ApiResponse;
 
-class CountSessions extends JsonResource
+class CountSessions extends JsonResource implements ApiResponse
 {
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
             'qty_sessions' => $this->resource
