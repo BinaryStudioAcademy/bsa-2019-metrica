1bf4dc8b7f0c95c9e133fa8fcf63f1ffa7299358
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-16 16:37:55 +0300

    convert Collection to integer

--- backend/app/Actions/Sessions/GetAvgSessionResponse.php
+++ backend/app/Actions/Sessions/GetAvgSessionResponse.php
@@ -3,18 +3,19 @@
 declare(strict_types=1);
 
 namespace App\Actions\Sessions;
+use Illuminate\Support\Collection;
 
 final class GetAvgSessionResponse
 {
     private $avgSession;
 
-    public function __construct($avgSession)
+    public function __construct(Collection $avgSession)
     {
         $this->avgSession = $avgSession;
     }
 
     public function avgSession(): int
     {
-        return $this->avgSession;
+        return (int) $this->avgSession->first()->avg;
     }
 }
