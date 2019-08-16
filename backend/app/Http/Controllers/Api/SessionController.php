96a7561d6c9881636539381e703a0e33779d251b
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-16 21:08:36 +0300

    add count of sessions endpoint

--- backend/app/Http/Controllers/Api/SessionController.php
+++ backend/app/Http/Controllers/Api/SessionController.php
@@ -8,14 +8,21 @@ use App\Actions\Sessions\GetAllSessionsAction;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SessionResourceCollection;
 use App\Http\Response\ApiResponse;
+use App\Http\Resources\CountSession;
+use App\Http\Requests\Api\CountSessionsHttpRequest;
 
 final class SessionController extends Controller
 {
     private $getAllSessionsAction;
+    private $countSessionsAction;
 
-    public function __construct(GetAllSessionsAction $getAllSessionsAction)
+    public function __construct(
+        GetAllSessionsAction $getAllSessionsAction,
+        CountSessionsAction $countSessionsAction
+    )
     {
         $this->getAllSessionsAction = $getAllSessionsAction;
+        $this->countSessionsAction = $countSessionsAction;
     }
 
     public function getAllSessions(): ApiResponse
@@ -23,5 +30,14 @@ final class SessionController extends Controller
         $response = $this->getAllSessionsAction->execute();
 
         return ApiResponse::success(new SessionResourceCollection($response->sessions()));
+    }
+
+    public function getCountOfSessions(CountSessionsHttpRequest $request): ApiResponse
+    {
+        $response = $this->countSessionAction->execute(
+            CountSessionsRequest::fromHttpRequest($request)
+        );
+
+        return ApiResponse::success(new CountSession($response->countSessions()));
     }
 }
