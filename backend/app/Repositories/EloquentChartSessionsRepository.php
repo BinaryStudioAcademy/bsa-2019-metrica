8a0291dbc9b0da971004e8019808d3ce0e148d6d
devhackerone <50499490+devhackerone@users.noreply.github.com>
2019-08-20 09:39:45 +0300

    Update EloquentChartSessionsRepository.php

--- backend/app/Repositories/EloquentChartSessionsRepository.php
+++ backend/app/Repositories/EloquentChartSessionsRepository.php
@@ -33,48 +33,53 @@ final class EloquentChartSessionsRepository implements ChartSessionsRepository
     public function findByFilter(DatePeriod $filterData, int $interval, int $websiteId): Collection
     {
 
-        // $subQuery = "SELECT s.*, (" . $this->roundDate('s.start_session', $interval) . ") as date " .
-        //     "FROM sessions AS s ".
-        //     "WHERE " .
-        //         "s.website_id = " . "$websiteId AND " .
-        //         $this->toTimestamp('s.start_session') . " >= :start_date AND " .
-        //         $this->toTimestamp('s.start_session') . " <= :end_date";
+         $subQuery = "SELECT s.*, (" . $this->roundDate('s.start_session', $interval) . ") as date " .
+             "FROM sessions AS s ".
+             "WHERE " .
+                 "s.website_id = " . "$websiteId AND " .
+             $this->toTimestamp('s.start_session') . " >= :start_date AND " .
+             $this->toTimestamp('s.start_session') . " <= :end_date OR ".
+             $this->toTimestamp('s.start_session') . " < :start_date AND " .
+             $this->toTimestamp('s.end_session') . " > :start_date"
+//                 $this->toTimestamp('s.start_session') . " >= :end_date AND " .
+//                 $this->toTimestamp('s.end_session') . " <= :end_date"
+         ;
 
-        // $query = DB::raw("SELECT COUNT(*) as sessions, date FROM ($subQuery) AS periods GROUP BY date");
+         $query = DB::raw("SELECT COUNT(*) as sessions, date FROM ($subQuery) AS periods GROUP BY date");
 
-        // $result =  DB::select((string)$query, [
-        //     'start_date' => $filterData->getStartDate()->getTimestamp(),
-        //     'end_date' =>  $filterData->getEndDate()->getTimestamp(),
-        // ]);
+         $result =  DB::select((string)$query, [
+             'start_date' => $filterData->getStartDate()->getTimestamp(),
+             'end_date' =>  $filterData->getEndDate()->getTimestamp(),
+         ]);
 
-        // return collect($result)->map(function ($item) {
-        //     return new ChartSessions($item->date, $item->sessions);
-        // });
+         return collect($result)->map(function ($item) {
+             return new ChartSessions($item->date, $item->sessions);
+         });
 
-        // todo filter by website
-        // 
-        $result = Session::whereDateBetween($filterData)
-            ->get()
-            ->reduce(function ($hashTable, $item) use ($interval) {
-                $startTimestamp = $item->start_session->getTimestamp();
-                $endTimestamp = $item->end_session->getTimestamp();
-                $startDate = $startTimestamp - ($startTimestamp % $interval) + $interval;
-                $endDate = $endTimestamp - ($endTimestamp % $interval) + $interval;
-                
-                for ($offset = $endDate - $startDate; $offset > 0; $offset -= $interval) {
-                    $position = $startDate + $offset;
-
-                    if (!isset($hashTable[$position]) || is_null($hashTable[$position])) {
-                        $hashTable[$position] = 0;
-                    }
-
-                    $hashTable[$position]++;
-                }
-
-                return $hashTable;
-            }, []);
-
-        return collect($result);
+//        $result = Session::where('website_id', $websiteId)
+//            ->whereDateBetween($filterData)
+//            ->get()
+//            ->reduce(function ($hashTable, $item) use ($interval) {
+//                $startTimestamp = $item->start_session->getTimestamp();
+//                $endTimestamp = $item->end_session->getTimestamp();
+//                $startDate = $startTimestamp - ($startTimestamp % $interval) + $interval;
+//                dd($startDate);
+//                $endDate = $endTimestamp - ($endTimestamp % $interval) + $interval;
+//
+//                for ($offset = $endDate - $startDate; $offset > 0; $offset -= $interval) {
+//                    $position = $startDate + $offset;
+//
+//                    if (!isset($hashTable[$position]) || is_null($hashTable[$position])) {
+//                        $hashTable[$position] = 0;
+//                    }
+//
+//                    $hashTable[$position]++;
+//                }
+//
+//                return $hashTable;
+//            }, []);
+//
+//        return collect($result);
     }
 }
 // app()->make(\App\Repositories\Contracts\ChartSessionsRepository::class)->findByFilter(new \App\Utils\DatePeriod(new DateTime('2019-08-19 06:00:00'), new DateTime('2019-08-19 08:00:00')), 3600, 2)
