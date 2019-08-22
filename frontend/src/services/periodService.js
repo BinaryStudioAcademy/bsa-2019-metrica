c6a734f1a82033d5c1ba4da1962253b5e9e6b439
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-22 10:04:09 +0300

    add constant

--- frontend/src/services/periodService.js
+++ frontend/src/services/periodService.js
@@ -1,5 +1,15 @@
 import moment from 'moment';
 
+export const period = {
+    PERIOD_TODAY: 'today',
+    PERIOD_YESTERDAY: 'yesterday',
+    PERIOD_LAST_WEEK: 'last_week',
+    PERIOD_LAST_MONTH: 'last_month',
+    PERIOD_LAST_QUARTAL: 'last_quartal',
+    PERIOD_ALL: 'all_period',
+};
+
+
 const currentDate = () => {
     return moment().format("DD-MM-YYYY HH:mm");
 };
@@ -28,39 +38,39 @@ const getWeekInterval = (week) => {
     return moment.duration({w: week}).asSeconds();
 };
 
-const getTimeByPeriod = (period) => {
+export const getTimeByPeriod = (period) => {
     let interval = null;
     let startDate = null;
     let endDate = null;
     switch (period) {
-        case 'today':
+        case period.PERIOD_TODAY:
             interval = getHourInterval(1);
             startDate = getSubtractStartDate(0);
             endDate = getSubtractEndDate(0);
             break;
-        case 'yesterday':
+        case period.PERIOD_YESTERDAY:
             interval = getHourInterval(1);
             startDate = getSubtractStartDate(1);
             endDate = getSubtractEndDate(1);
             break;
-        case 'last_week':
+        case period.PERIOD_LAST_WEEK:
             interval = getDayInterval(1);
             startDate = getSubtractStartDate(7);
             endDate = currentDate();
             break;
-        case 'last_month':
+        case period.PERIOD_LAST_MONTH:
             interval = getWeekInterval(1);
             startDate = getSubtractStartMonth(1);
             endDate = currentDate();
             break;
-        case 'last_quartal':
+        case period.PERIOD_LAST_QUARTAL:
             interval = getDayInterval(30);
             startDate = getSubtractStartMonth(3);
             endDate = currentDate();
             break;
-        case 'all_period':
+        case period.PERIOD_ALL:
             interval = getDayInterval(30);
-            startDate = NaN;
+            startDate = undefined;
             endDate = currentDate();
             break;
         default:
@@ -68,14 +78,8 @@ const getTimeByPeriod = (period) => {
     }
 
     return {
-        interval: interval,
-        startDate: startDate,
-        endDate: endDate
+        interval,
+        startDate,
+        endDate
     };
 };
-
-const periodService = {
-    getTimeByPeriod
-};
-
-export default periodService;
