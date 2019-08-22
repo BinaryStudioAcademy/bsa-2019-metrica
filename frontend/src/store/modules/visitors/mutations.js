98589aad651b32236d75f1084af2f0a0c308974e
devhackerone <50499490+devhackerone@users.noreply.github.com>
2019-08-22 22:50:42 +0300

    add mutations

--- frontend/src/store/modules/visitors/mutations.js
+++ frontend/src/store/modules/visitors/mutations.js
@@ -1,4 +1,13 @@
-import {SET_SELECTED_PERIOD, SET_ACTIVE_BUTTON, SET_BUTTON_FETCHING, RESET_BUTTON_FETCHING} from "./types/mutations";
+import {
+    SET_SELECTED_PERIOD,
+    SET_ACTIVE_BUTTON,
+    SET_BUTTON_FETCHING,
+    RESET_BUTTON_FETCHING,
+    SET_LINE_CHART_DATA,
+    GET_SELECTED_PERIOD,
+    SET_LINE_CHART_DATA_FETCHING,
+    RESET_LINE_CHART_DATA_FETCHING
+} from "./types/mutations";
 
 export default {
     [SET_SELECTED_PERIOD]: (state, period) => {
@@ -12,5 +21,17 @@ export default {
     },
     [RESET_BUTTON_FETCHING]: (state, button) => {
         state.buttonData[button].isFetching = false;
+    },
+    [GET_SELECTED_PERIOD]: (state) => {
+        return state.selectedPeriod;
+    },
+    [SET_LINE_CHART_DATA]: (state, value) => {
+        state.chartData.items = value;
+    },
+    [SET_LINE_CHART_DATA_FETCHING]: (state) => {
+        state.chartData.isFetching = true;
+    },
+    [RESET_LINE_CHART_DATA_FETCHING]: (state) => {
+        state.chartData.isFetching = false;
     },
 };
