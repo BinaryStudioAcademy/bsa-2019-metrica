23da8738e557f42f0856b34d7e370868725941d6
Konstantin Rybakov <k.ribakov@gmail.com>
2019-08-23 09:56:41 +0300

    change resource

--- backend/app/Http/Resources/VisitorResource.php
+++ backend/app/Http/Resources/VisitorResource.php
@@ -6,13 +6,21 @@ namespace App\Http\Resources;
 
 use App\Contracts\ApiResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 final class VisitorResource extends JsonResource implements ApiResponse
 {
     public function toArray($request): array
     {
         return [
-            // TODO
+            'id' => $this->resource,
+            'token' => $this->getJWTToken()
         ];
+    }
+
+    private function getJWTToken(): string
+    {
+        return JWTAuth::fromUser(auth()->user());
     }
 }
