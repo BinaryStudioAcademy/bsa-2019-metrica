55156dfb0570a2302235f156245e1ce9e861748c
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-26 00:05:23 +0300

    add echo service

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,14 @@
 const getApiUrl = () => process.env.VUE_APP_API_URL;
 const getGoogleMapsApiKey = () => process.env.GOOGLE_MAPS_API_KEY;
+const getPusherApiKey = () => process.env.PUSHER_APP_KEY;
+const getPusherCluster = () => process.env.PUSHER_CLUSTER;
+const getPusherAppAuthEndpoint = () => process.env.PUSHER_APP_AUTH_ENDPOINT;
 
 export default {
   getApiUrl,
-    getGoogleMapsApiKey
+  getGoogleMapsApiKey,
+  getPusherApiKey,
+  getPusherCluster,
+  getPusherAppAuthEndpoint
+
 };
---
+++ frontend/src/services/echoService.js
@@ -0,0 +1,19 @@
+import Echo from 'laravel-echo';
+import Storage from '@/services/Storage';
+import config from "@/config";
+
+const configPusher = {
+    broadcaster: 'pusher',
+    key: config.getPusherApiKey(),
+    authEndpoint: config.getPusherAppAuthEndpoint(),
+    cluster: config.getPusherCluster(),
+    encrypted: true,
+    auth: {
+        headers: {
+            Authorization: 'Bearer ' + Storage.getToken()
+        },
+    },
+};
+
+export const pusher = new Echo(configPusher);
+export const getSocketId = () => pusher.socketId();
