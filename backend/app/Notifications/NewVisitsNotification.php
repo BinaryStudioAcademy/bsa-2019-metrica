4cd0817f78c66e6d4719b2aeb12ce547db06f750
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-28 22:32:05 +0300

    edit style code

--- backend/app/Notifications/NewVisitsNotification.php
+++ backend/app/Notifications/NewVisitsNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Notification;
 
-class NewVisitsNotification  extends Notification implements ShouldQueue
+class NewVisitsNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
