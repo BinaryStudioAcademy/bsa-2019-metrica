8ee465d1cd86d8b83c84b5fc4416b8a804077d1c
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-08-30 17:02:27 +0300

    edit style code

--- backend/app/Providers/BroadcastServiceProvider.php
+++ backend/app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,6 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         Broadcast::routes([ 'middleware' => ['auth:api', \Barryvdh\Cors\HandleCors::class] ]);
 
         require base_path('routes/channels.php');
