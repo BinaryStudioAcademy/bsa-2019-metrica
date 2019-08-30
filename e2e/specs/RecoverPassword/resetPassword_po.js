0090a68f84cdfd5d73b0d19cca4e33534b64726a
Volodymyr Lench <volodymyr.lench@binary-studio.com>
2019-08-31 00:07:28 +0300

    fix extra semicolons

--- e2e/specs/RecoverPassword/resetPassword_po.js
+++ e2e/specs/RecoverPassword/resetPassword_po.js
@@ -1,15 +1,15 @@
 class ResetPasswordPage {
     
-    get emailField () {return $('input[name="email"]');};
+    get emailField () {return $('input[name="email"]');}
     
-    get resetPasswordButton () {return $('button.primary');};
+    get resetPasswordButton () {return $('button.primary');}
 
-    get titleText () {return $('div.v-subheader');};   
-    get errorText() {return $('div[role="alert"]');};
+    get titleText () {return $('div.v-subheader');}   
+    get errorText() {return $('div[role="alert"]');}
 
-    get emailErrorLable (){return $('div.v-messages__message');};
+    get emailErrorLable (){return $('div.v-messages__message');}
     
-    get messageArea (){return $('div.v-alert__content');};
-};
+    get messageArea (){return $('div.v-alert__content');}
+}
 
 module.exports = ResetPasswordPage;
