4e28ab63d4a0f6c4072a19b317f6e7704f357086
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2019-08-30 15:27:05 +0300

    fix codacy warnings

--- frontend/src/services/jwtService.js
+++ frontend/src/services/jwtService.js
@@ -2,7 +2,7 @@ import jwt_decode from "jwt-decode";
 
 const parse = (token, options = {}) => jwt_decode(token, options);
 const checkExpireToken = token => {
-    let parsedToken = jwtService.parse(token);
+    let parsedToken = parse(token);
     return parsedToken.exp < Date.now().valueOf() / 1000;
 };
 const jwtService = {
