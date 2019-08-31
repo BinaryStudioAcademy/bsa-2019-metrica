9b0f11e4e81e891da263caadb91efa6d9a353b30
Serhii Plotnikov <serhiiplotnikov1934@gmail.com>
2019-08-31 15:52:30 +0300

    divide sql query into smaller methods

--- backend/app/Repositories/EloquentChartSessionsRepository.php
+++ backend/app/Repositories/EloquentChartSessionsRepository.php
@@ -15,10 +15,9 @@ final class EloquentChartSessionsRepository implements ChartSessionsRepository
 
     public function findByFilter(DatePeriod $datePeriod, int $interval, int $websiteId): Collection
     {
-        $result = DB::select("select COUNT(*) as count, extract(epoch from :startDate::timestamp)+$interval*(
-            DIV((extract(epoch from start_session) - 
-                    extract(epoch from :startDate::timestamp))::numeric,$interval)) as period
-                FROM \"sessions\"
+        $result = DB::select("select COUNT(*) as count, " . $this->getPeriod('start_session', $interval) . "
+          as period
+             FROM \"sessions\"
                 WHERE website_id=:websiteId AND start_session BETWEEN :startDate AND :endDate
             GROUP BY period
                ORDER BY period", [
@@ -29,5 +28,22 @@ final class EloquentChartSessionsRepository implements ChartSessionsRepository
         return collect($result)->map(function ($item) {
             return new ChartValue($item->period, (string)$item->count);
         });
+    }
+
+    private function toTimestamp(string $columnName): string
+    {
+        return "extract(epoch from {$columnName})";
+    }
+
+    private function toNumeric(string $expression): string
+    {
+        return "({$expression})::numeric";
+    }
+
+    private function getPeriod(string $columnName, int $interval): string
+    {
+        return $this->toTimestamp(":startDate::timestamp") . "+$interval*(
+                  DIV(" . $this->toNumeric($this->toTimestamp($columnName) . " - " .
+                $this->toTimestamp(":startDate::timestamp")) . ",{$interval}))";
     }
 }
