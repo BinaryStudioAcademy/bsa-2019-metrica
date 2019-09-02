0a8a79c0abded685ce21d0997747e11fb446ec5f
TumkoVolodymyr <tumko.volodymyr@gmail.com>
2019-09-02 10:54:45 +0000

    Apply fixes from StyleCI

--- backend/app/Http/Requests/PageTimings/PageTimingChartHttpRequest.php
+++ backend/app/Http/Requests/PageTimings/PageTimingChartHttpRequest.php
@@ -6,7 +6,7 @@ namespace App\Http\Requests\PageTimings;
 use App\Http\Request\ApiFormRequest;
 use App\Http\Requests\ChartHttpRequestTrait;
 
-final class PageTimingChartHttpRequest  extends ApiFormRequest
+final class PageTimingChartHttpRequest extends ApiFormRequest
 {
     use ChartHttpRequestTrait;
 }
