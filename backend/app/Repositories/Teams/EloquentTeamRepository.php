bfb5d1b9f28a6a7c59c856cbb5f9511344293bd2
Eduard <motrii.edu@gmail.com>
2019-09-02 19:54:03 +0300

    add endpoint teams

--- backend/app/Repositories/Teams/EloquentTeamRepository.php
+++ backend/app/Repositories/Teams/EloquentTeamRepository.php
@@ -13,18 +13,10 @@ final class EloquentTeamRepository implements TeamRepository
     public function getTeamMembers(int $websiteId): Collection
     {
         return DB::table('users')
+            ->websites()
+            ->wherePivot('website_id', '=', $websiteId)
+            ->wherePivot('role', '=', 'member')
             ->select('name', 'email')
-            ->whereHas('websites', function ($q) use ($websiteId) {
-                $q->where('id', '=', $websiteId);
-            })
-            ->find($websiteId)
-            ->users()
-            ->wherePivot('roles', 'member')
             ->get();
-//            ->has('us')
-//            ->join('users', 'users_id')
-//            ->join('user_role', 'websites.user_id', '=', 'user_role.user_id')
-//            ->select()
-//            ->where('user_role.role_id', '=', 2)
     }
 }
