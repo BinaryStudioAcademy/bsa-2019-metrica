6a89c3a17b0171d2622a9c0e29d24670f376b7b6
naytis <binaural.omega@gmail.com>
2019-09-06 00:06:42 +0300

    add custom scrollbar

--- frontend/src/components/dashboard/visitors-flow/VisitorsFlowDiagram.vue
+++ frontend/src/components/dashboard/visitors-flow/VisitorsFlowDiagram.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="visitors-flow-container">
+        <div class="overflow" />
         <svg id="visitors-flow-diagram" />
         <div class="step">
             <button
@@ -212,6 +213,34 @@
         max-width: 70vw;
         overflow-x: auto;
         transition: all .5s;
+
+        &::-webkit-scrollbar {
+            background-color:#fff;
+            width:16px
+        }
+
+        &::-webkit-scrollbar-track {
+             background-color:#fff
+        }
+
+        &::-webkit-scrollbar-track:hover {
+            background-color:#f4f4f4
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background-color:#babac0;
+            border-radius:16px;
+            border:5px solid #fff
+        }
+
+        &::-webkit-scrollbar-thumb:hover {
+            background-color:#a0a0a5;
+            border:4px solid #f4f4f4
+        }
+
+        &::-webkit-scrollbar-button {
+            display:none
+        }
 
         #visitors-flow-diagram {
 
