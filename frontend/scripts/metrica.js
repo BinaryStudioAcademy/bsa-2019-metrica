a0890439c5bbfbf38b6a5458a52a43144747a7ab
UMarina <m.udovitskaya@gmail.com>
2019-09-05 14:36:48 +0300

    fix CORS in script

--- frontend/scripts/metrica.js
+++ frontend/scripts/metrica.js
@@ -3,7 +3,7 @@
         post(url, data, headers = {}) {
             return fetch(url, {
                 method: 'POST',
-                crossDomain: true,
+                mode: 'cors',
                 headers: headers,
                 body: JSON.stringify(data)
             });
