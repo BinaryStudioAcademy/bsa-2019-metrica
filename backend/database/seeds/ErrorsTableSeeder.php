d082d7f74f2245b62ef522f9b2b02deb9d0b2b72
UMarina <m.udovitskaya@gmail.com>
2019-09-05 10:30:10 +0300

    fix global seeder and ad new files

---
+++ backend/database/seeds/ErrorsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Entities\User;
+use App\Entities\Error;
+
+class ErrorsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $user = User::query()->where('email', '=', 'info@metrica.fun')->first();
+
+        for ($i = 0; $i < 5; $i++) {
+            $visit = $user->website->visits->random();
+            factory(Error::class,5)->create([
+                'visitor_id' => $visit->visitor->id,
+                'page_id' => $visit->page->id
+            ]);
+        }
+    }
+}
