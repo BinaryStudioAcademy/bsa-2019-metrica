5aaa63da61ef824dee6178a8e7c384e2000c98ad
UMarina <m.udovitskaya@gmail.com>
2019-09-06 08:16:31 +0300

    add table component

--- frontend/src/pages/speed_overview/ErrorReports.vue
+++ frontend/src/pages/speed_overview/ErrorReports.vue
@@ -4,11 +4,14 @@
 
 <script>
     import ContentLayout from '../../components/layout/ContentLayout.vue';
+    import { isWebsite } from "@/mixins/isWebsite";
+
     export default {
         name: "ErrorReports",
         components: {
             ContentLayout,
         },
+        mixins: [isWebsite],
         data() {
             return {
                 title: "Error Reports",
